269a4375bfbf1edac83dc20895d907dbcb686d4f
ChrisDaVillager <chrispuccetti2@gmail.com>
2022-06-20 16:40:54 -0700

    Added and adjusted list

--- animelists.py
+++ animelists.py
@@ -1,5 +1,6 @@
 animes = ['Ranking of Queens', 'Demon SLayer', 'Tokyo Ghoul']
 print(animes)
+# Output: ['Ranking of Queens', 'Demon SLayer', 'Tokyo Ghoul']
 
 for x in range(len(animes)):
     if animes[x] == 'Ranking of Queens':
@@ -7,9 +8,12 @@ for x in range(len(animes)):
     elif animes[x] == 'Demon SLayer':
         animes[x] = 'Demon Slayer'
 print(animes)
+# Output: ['Ranking of Kings', 'Demon Slayer', 'Tokyo Ghoul']
 
 animes.insert(0, 'Spy x Family')
 print(animes)
+# Output: ['Spy x Family', 'Ranking of Kings', 'Demon Slayer', 'Tokyo Ghoul']
 
 animes.append('Overlord')
 print(animes)
+# Output: ['Spy x Family', 'Ranking of Kings', 'Demon Slayer', 'Tokyo Ghoul', 'Overlord']
